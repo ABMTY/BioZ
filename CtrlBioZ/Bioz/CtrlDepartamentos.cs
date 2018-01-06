78f76a6d27f36ee67bdfaf0336301f54c9b3f329
Dperezs <dev.danielp@abmty.com.mx>
2018-01-06 10:53:38 -0600

    Persistencia,Control y Controller de Departamento

---
+++ CtrlBioZ/Bioz/CtrlDepartamentos.cs
@@ -0,0 +1,45 @@
+﻿using EntBioZ.Modelo.BioZ;
+using PerBioZ.Bioz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtrlBioZ.Bioz
+{
+    public class CtrlDepartamentos
+    {
+        PerDepartamentos PerDepartamentos;
+
+        public List<EntDepartamento> Lista;
+
+        public CtrlDepartamentos()
+        {
+            PerDepartamentos = new PerDepartamentos();
+        }
+        public List<EntDepartamento> ObtenerTodos()
+        {
+            return (List<EntDepartamento>)new PerDepartamentos().ObtenerTodos();
+        }
+        public EntDepartamento Obtener(int Id_Rol)
+        {
+            return (EntDepartamento)new PerDepartamentos().Obtener(Id_Rol);
+        }
+
+        public bool Insertar(EntDepartamento Entidad)
+        {
+            return PerDepartamentos.Insert(Entidad);
+        }
+
+        public bool Actualizar(EntDepartamento Entidad)
+        {
+            return PerDepartamentos.Update(Entidad);
+        }
+
+        //public bool Eliminar(int Id_Vistas)
+        //{
+        //    return PerVistas.Eliminar(Id_Vistas);
+        //}
+    }
+}
---
+++ PerBioZ/Bioz/PerDepartamentos.cs
@@ -0,0 +1,154 @@
+﻿using EntBioZ.Modelo.BioZ;
+using IBM.Data.Informix;
+using PerBioZ.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerBioZ.Bioz
+{
+    public class PerDepartamentos : Persistencia
+    {
+        public List<EntDepartamento> ObtenerTodos()
+        {
+            List<EntDepartamento> Lista = new List<EntDepartamento>();
+            EntDepartamento entidad = null;
+            try
+            {
+                AbrirConexion();
+                StringBuilder CadenaSql = new StringBuilder();
+                var sql = "SELECT id_departamento, desc_departamento FROM informix.departamentos";
+                IfxCommand cmd = new IfxCommand(sql, Conexion);
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        entidad = new EntDepartamento();
+                        entidad.id_departamento = int.Parse(dr["id_departamento"].ToString());
+                        entidad.desc_departamento = dr["desc_departamento"].ToString();
+                        Lista.Add(entidad);
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                throw exc;
+            }
+            finally
+            {
+                CerrarConexion();
+            }
+            return Lista;
+
+        }
+        public EntDepartamento Obtener(int id)
+        {
+            EntDepartamento entidad = null;
+            try
+            {
+                AbrirConexion();
+                StringBuilder CadenaSql = new StringBuilder();
+
+                IfxCommand cmd = new IfxCommand(string.Empty, Conexion);
+                cmd.CommandText = "SELECT id_departamento, desc_departamento FROM informix.departamentos WHERE id_departamento=?";
+                cmd.Parameters.Add(new IfxParameter()).Value = id;
+                using (var dr = cmd.ExecuteReader())
+                {
+                    if (dr.Read())
+                    {
+                        entidad = new EntDepartamento();
+                        entidad.id_departamento = int.Parse(dr["id_departamento"].ToString());
+                        entidad.desc_departamento = dr["desc_departamento"].ToString();
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                throw exc;
+            }
+            finally
+            {
+                CerrarConexion();
+            }
+            return entidad;
+
+        }
+        public bool Insert(EntDepartamento entidad)
+        {
+            bool respuesta = false;
+            try
+            {
+                AbrirConexion();
+                var sql = "execute procedure dml_departamentos (?,NULL,?);";
+                using (var cmd = new IfxCommand(sql, Conexion))
+                {
+                    cmd.Connection = Conexion;
+                    cmd.Parameters.Add(new IfxParameter()).Value = "INSERT";
+                    cmd.Parameters.Add(new IfxParameter()).Value = entidad.desc_departamento;
+                    cmd.ExecuteNonQuery();
+                }
+                respuesta = true;
+
+            }
+
+            catch (InvalidCastException ex)
+            {
+                ApplicationException excepcion = new ApplicationException("Se genero un error con el siguiente mensaje: " + ex.Message, ex);
+                excepcion.Source = "Insert Departamentos";
+                throw excepcion;
+            }
+            catch (Exception ex)
+            {
+                ApplicationException excepcion = new ApplicationException("Se genero un error de aplicación con el siguiente mensaje: " + ex.Message, ex);
+                excepcion.Source = "Insert Departamentos";
+                throw excepcion;
+            }
+            finally
+            {
+                CerrarConexion();
+            }
+            return respuesta;
+
+        }
+        public bool Update(EntDepartamento entidad)
+        {
+            bool respuesta = false;
+            try
+            {
+                AbrirConexion();
+                var sql = "execute procedure dml_departamentos (?,?,?);";
+                using (var cmd = new IfxCommand(sql, Conexion))
+                {
+                    cmd.Connection = Conexion;
+                    cmd.Parameters.Add(new IfxParameter()).Value = "UPDATE";
+                    cmd.Parameters.Add(new IfxParameter()).Value = entidad.id_departamento;
+                    cmd.Parameters.Add(new IfxParameter()).Value = entidad.desc_departamento;
+                    cmd.ExecuteNonQuery();
+                }
+                respuesta = true;
+
+            }
+
+            catch (InvalidCastException ex)
+            {
+                ApplicationException excepcion = new ApplicationException("Se genero un error con el siguiente mensaje: " + ex.Message, ex);
+                excepcion.Source = "Update Departamentos";
+                throw excepcion;
+            }
+            catch (Exception ex)
+            {
+                ApplicationException excepcion = new ApplicationException("Se genero un error de aplicación con el siguiente mensaje: " + ex.Message, ex);
+                excepcion.Source = "Update Departamentos";
+                throw excepcion;
+            }
+            finally
+            {
+                CerrarConexion();
+            }
+            return respuesta;
+
+        }
+    }
+}
