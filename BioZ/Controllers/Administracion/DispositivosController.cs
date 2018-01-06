b9da7428a1e7a240adae38f385f18bd8b1db61d4
LuisAngel <LuisAngel@DESKTOP-5CSNEIK>
2018-01-06 13:06:37 -0600

    Creacion del Controla y Vista de Dispositivos y Jornadas

---
+++ BioZ/Controllers/Administracion/DispositivosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BioZ.Controllers.Administracion
+{
+    public class DispositivosController : Controller
+    {
+        // GET: Dispositivos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
