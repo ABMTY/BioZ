c4dc338a7139e4707173c21a21d1086fb16b9992
Alvarad0 <alvarado.programador@gmail.com>
2018-01-09 10:38:06 -0600

    Funciones Insertar y Actualizar al modulo de Empleados

--- BioZ/Content/funcionesJs/empleado.js
+++ BioZ/Content/funcionesJs/empleado.js
@@ -1,116 +1,205 @@
-﻿//Variable Global
-//var tabla;
-//Función que se ejecutara al inicio
-function init() {
-    mostrarForm(false);
-    //listar();
-    $("#formulario").on("submit", function (e) {
-        //guardaryeditar(e);
+﻿$(document).ready(function () {
+    $("#secc_form").hide();
+    $('#tbllistado').DataTable();
+    listar();
+});
+
+//Listar Empleado
+function listar() {
+    $('#tbllistado').DataTable({
+        destroy: true,
+        searching: true,
+        "ajax": {
+            "url": "/Empleado/GetEmpleados/",
+            "type": "GET"
+        },
+        "columns": [
+            { "defaultContent": "" },
+            { "data": "nombre" },
+            { "data": "nombre_completo" },
+            { "data": "desc_departamento" },
+            { "data": "desc_sucursal" },
+            { "data": "enrollnumber"}
+        ],
+        "columnDefs": [{
+            "targets": 0, "data": "id_empleado", "render": function (data, type, full, meta) {
+                return "<button type='button' title='Editar' id='btn_mas" + data + "' class='btn btn-warning' onclick='verDetalle(" + data + ")'  ><i class='fa fa-edit'></i></button>"
+            }
+        }]
+    });
+}
+
+//Listar Departamentos
+function listarDepartamentos() {
+    $.ajax({
+        url: "/Departamento/GetDepartamentos/",
+        async: true,
+        beforeSend: function () { },
+        dataType: "json",
+        data: '{ }',
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            console.log(data);
+            $.each(data.data, function (i, Item) {
+                $("#departamento").append("<option value='" + Item.id_departamento + "'>" + Item.desc_departamento + "</option>")
+            });
+            $('#departamento').selectpicker('refresh');
+        },
+        xhr: function () {
+            var xhr = $.ajaxSettings.xhr();
+            xhr.onprogress = function (evt) {
+                var porcentaje = Math.floor((evt.loaded / evt.total * 100));
+            };
+            return xhr;
+        },
+        error: function (request, status, error) {
+            console.log("Error => " + error);
+        }
     })
 }
-//Función para limpiar los campos del formulario
-function limpiar() {
-    $("#nombre").val("");
-    $("#ap_pat").val("");
-    $("#ap_mat").val("");
-    $("#departamento").val("");
-    $("#sucursal").val("");
+
+//Listar Sucursales
+function listarSucursales() {
+    $.ajax({
+        url: "/Sucursal/GetSucursales/",
+        async: true,
+        beforeSend: function () { },
+        dataType: "json",
+        data: '{ }',
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            console.log(data);
+            $.each(data.data, function (i, Item) {
+                $("#sucursal").append("<option value='" + Item.id_sucursal + "'>" + Item.desc_sucursal + "</option>")
+            });
+            $('#sucursal').selectpicker('refresh');
+        },
+        xhr: function () {
+            var xhr = $.ajaxSettings.xhr();
+            xhr.onprogress = function (evt) {
+                var porcentaje = Math.floor((evt.loaded / evt.total * 100));
+            };
+            return xhr;
+        },
+        error: function (request, status, error) {
+            console.log("Error => " + error);
+        }
+    })
 }
-//Función Mostrar Formulario
-function mostrarForm(flag) {
+
+//Mostrar Formulario de Agregar
+function Agregar() {
+    $("#listadoregistros").hide();
+    $("#secc_form").show();
+    $("#btnAgregar").hide();
     limpiar();
-    if (flag) {
-        $("#listadoregistros").hide();
-        $("#formularioregistros").show();
-        $("#btnGuardar").prop("disabled", false);
-        $("#btnAgregar").hide();
-    } else {
-        $("#listadoregistros").show();
-        $("#formularioregistros").hide();
-        $("#btnAgregar").show();
-    }
+    listarDepartamentos();
+    listarSucursales();
 }
-//Función Cancelar Formulario
+
+//No envia los datos del formulario y carga la tabla
 function cancelarForm() {
+    $("#listadoregistros").show();
+    $("#secc_form").hide();
+    $("#btnAgregar").show();
     limpiar();
-    mostrarForm(false);
 }
-//Función Listar
-//function listar() {
-//    tabla = $("#tbllistado").dataTable({
-//        "aProcessing": true, //Activa Procesamiento de DataTables
-//        "aServerSide": true, //Paginación y filtrado realizado por el servidor
-//        dom: "Bfrtip",
-//        buttons: [
-//            "copyHtml5",
-//            "excelHtml5",
-//            "csvHtml5",
-//            "pdf"
-//        ],
-//        "ajax": {
-//            url: "../ajax/categoria.php?op=listar",
-//            type: "get",
-//            dataType: "json",
-//            error: function (e) {
-//                console.log(e.responseText);
-//            }
-//        },
-//        "bDestroy": true,
-//        "iDisplayLength": 5,
-//        "order": [[0, "desc"]]
-//    }).DataTable();
-//}
-////Función Guardar o Editar
-//function guardaryeditar(e) {
-//    e.preventDefault(); //No se activará la acción predeterminada del botón
-//    $("#btnGuardar").prop("disabled", true);
-//    var formData = new FormData($("#formulario")[0]);
-//    $.ajax({
-//        url: "../ajax/categoria.php?op=guardaryeditar",
-//        type: "POST",
-//        data: formData,
-//        contentType: false,
-//        processData: false,
-//        success: function (datos) {
-//            swal(datos, "", "success");
-//            //alert(datos);
-//            mostrarForm(false);
-//            tabla.ajax.reload();
-//        }
-//    });
-//    limpiar();
-//}
-////Función para Mostrar en el formulario los datos que se enviaran a Editar
-//function mostrar(idcategoria) {
-//    $.post("../ajax/categoria.php?op=mostrar", { idcategoria: idcategoria }, function (data, status) {
-//        data = JSON.parse(data);
-//        mostrarForm(true);
-//        $("#idcategoria").val(data.idcategoria);
-//        $("#nombre").val(data.nombre);
-//        $("#descripcion").val(data.descripcion);
-//    });
-//}
-////Funcion para Activar Categoría
-//function activar(idcategoria) {
-//    swal({
-//        title: '¿Está seguro que desea activar la categoría?',
-//        text: "",
-//        type: 'warning',
-//        showCancelButton: true,
-//        confirmButtonColor: '#3085d6',
-//        cancelButtonColor: '#d33',
-//        confirmButtonText: 'Confirmar'
-//    }).then((result) => {
-//        if (result.value) {
-//            $.post("../ajax/categoria.php?op=activar", { idcategoria: idcategoria }, function (e) {
-//                tabla.ajax.reload();
-//                swal(
-//                    e,
-//                    '',
-//                    'success'
-//                )
-//            })
-//        }
-//    })
-//}
-init();
+
+//Limpia los campos del formulario
+function limpiar() {
+    $("#id_empleado").val("");
+    $("#nombre").val("");
+    $("#ap_paterno").val("");
+    $("#ap_materno").val("");
+    $("#id_departamento").val("");
+    $("#id_sucursal").val("");
+    $("#enrollnumber").val("");
+    $("#sucursal").html("");
+    $("#departamento").html("");
+}
+
+//Funcion para Guardar y Editar Empresa
+function guardarEditar() {
+    var e = document.getElementById("departamento");
+    var departamento = e.options[e.selectedIndex].value;
+    $("#id_departamento").val(departamento);
+
+    var s = document.getElementById("sucursal");
+    var sucursal = s.options[s.selectedIndex].value;
+    $("#id_sucursal").val(sucursal);
+
+    parametros = {
+        "id_empleado": $("#id_empleado").val(),
+        "nombre": $("#nombre").val(),
+        "ap_paterno": $("#ap_paterno").val(),
+        "ap_materno": $("#ap_materno").val(),
+        "id_departamento": $("#id_departamento").val(),
+        "id_sucursal": $("#id_sucursal").val(),
+        "enrollnumber": $("#enrollnumber").val()
+    }
+    $.ajax({
+        url: "/Empleado/Guardar/",
+        async: true,
+        dataType: "json",
+        beforeSend: function () { },
+        data: JSON.stringify(parametros),
+        cache: false,
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            cancelarForm();
+            swal({ title: "Empleado", text: "Registrado Correctamente", type: "success" }, function () { listar(); limpiar();});
+        },
+        error: function (request, status, error) {
+            +
+                cancelarForm();
+            swal({ title: "Error", text: "al guardar el Empleado", type: "error" }, function () { listar(); limpiar(); });
+        }
+    });
+}
+
+//Funcion Editar Sucursal
+function verDetalle(id_empleado) {
+    $("#listadoregistros").hide();
+    $("#secc_form").show();
+    $("#btnAgregar").hide();
+    $.ajax({
+        url: "/Empleado/GetEmpleado/" + id_empleado,
+        async: true,
+        beforeSend: function () { },
+        dataType: "json",
+        data: '{ }',
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            console.log(data);
+            $("#departamento").html("");
+            $("#sucursal").html("");
+            $("#id_empleado").val(data.data.id_empleado);
+            $("#id_departamento").val(data.data.id_departamento);
+            $("#id_sucursal").val(data.data.id_sucursal);
+            $("#nombre").val(data.data.nombre);
+            $("#ap_paterno").val(data.data.ap_paterno);
+            $("#ap_materno").val(data.data.ap_materno);
+            $("#enrollnumber").val(data.data.enrollnumber);
+            listarDepartamentos();
+            $("#departamento").append("<option value=" + data.data.id_departamento + ">" + data.data.desc_departamento + "</option>");
+            $("#departamento").selectpicker('refresh');
+            listarSucursales();
+            $("#sucursal").append("<option value=" + data.data.id_sucursal + ">" + data.data.desc_sucursal + "</option>");
+            $("#sucursal").selectpicker('refresh');
+        },
+        xhr: function () {
+            var xhr = $.ajaxSettings.xhr();
+            xhr.onprogress = function (evt) {
+                var porcentaje = Math.floor((evt.loaded / evt.total * 100));
+            };
+            return xhr;
+        },
+        error: function (request, status, error) {
+            console.log("Error => " + error);
+        }
+    })
+}
