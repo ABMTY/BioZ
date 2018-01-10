05184c89612a2993d981e2f99ff3d8ad043b8984
Dperezs <dev.danielp@abmty.com.mx>
2018-01-10 10:07:00 -0600

    Cambio de Tipo de datos de la propiedad Selected De Bool a String

--- EntBioZ/Modelo/Seguridad/EntVistas.cs
+++ EntBioZ/Modelo/Seguridad/EntVistas.cs
@@ -12,6 +12,6 @@ namespace EntBioZ.Modelo.Seguridad
         public int id_vista { get; set; }
         public string ventana { get; set; }
         public string url { get; set; }
-        public bool selected { get; set; }
+        public string selected { get; set; }
     }
 }
