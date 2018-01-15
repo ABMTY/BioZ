a7cfa09878073c5332043085758a2d32b1cff623
Dperezs <dev.danielp@abmty.com.mx>
2018-01-15 11:47:54 -0600

    Entidad, Persistencia y Control de Horarios y Turno

---
+++ CtrlBioZ/Bioz/CtrlHorarios.cs
@@ -0,0 +1,45 @@
+﻿using EntBioZ.Modelo.BioZ;
+using PerBioZ.Bioz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtrlBioZ.Bioz
+{
+    public class CtrlHorarios
+    {
+        PerHorario PerHorario;
+
+        public List<EntHorario> Lista;
+
+        public CtrlHorarios()
+        {
+            PerHorario = new PerHorario();
+        }
+        public List<EntHorario> ObtenerTodos()
+        {
+            return (List<EntHorario>)new PerHorario().ObtenerTodos();
+        }
+        public EntHorario Obtener(int Id_Rol)
+        {
+            return (EntHorario)new PerHorario().Obtener(Id_Rol);
+        }
+
+        public bool Insertar(EntHorario Entidad)
+        {
+            return PerHorario.Insert(Entidad);
+        }
+
+        public bool Actualizar(EntHorario Entidad)
+        {
+            return PerHorario.Update(Entidad);
+        }
+
+        public bool Eliminar(int IdHorario)
+        {
+            return PerHorario.Eliminar(IdHorario);
+        }
+    }
+}
---
+++ EntBioZ/Modelo/BioZ/EntHorario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntBioZ.Modelo.BioZ
+{
+    public class EntHorario
+    {
+        public int id_horario { get; set; }
+        public string desc_horario { get; set; }
+        public string hora_entrada { get; set; }
+        public string hora_salida { get; set; }
+        public string total_horas { get; set; }
+    }
+}
